82f465bda1c0261ae6ca7041726a7a18f3ac65c4
lisen <suilei@agora.io>
2019-05-16 21:26:46 +0800

    add upload file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import render_template,send_from_directory,Flask,request,jsonify,Response
+from flask import render_template,send_from_directory,Flask,request,jsonify,Response,abort,session
+from werkzeug.utils import secure_filename
 from flask_bootstrap import Bootstrap
-import os
+import os,time,base64
 from script.lib import *
 from script.countTestPlan import *
 app = Flask(__name__)
@@ -114,5 +115,43 @@ def getProcessChart():
     content = json.dumps(datas)
     resp = Response_headers(content)
     return resp
+
+
+UPLOAD_FOLDER = 'upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+basedir = os.path.abspath(os.path.dirname(__file__))
+ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'xls', 'JPG', 'PNG', 'xlsx', 'gif', 'GIF'])
+
+
+# 用于判断文件后缀
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+# 上传文件
+@app.route('/upload', methods=['POST'], strict_slashes=False)
+def api_upload():
+    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    f = request.files['myfile']  # 从表单的file字段获取文件，myfile为该表单的name值
+    if f and allowed_file(f.filename):  # 判断是否是允许上传的文件类型
+        fname = secure_filename(f.filename)
+        ext = fname.rsplit('.', 1)[1]  # 获取文件后缀
+        unix_time = int(time.time())
+        new_filename = str(unix_time) + '.' + ext  # 修改了上传的文件名
+        f.save(os.path.join(file_dir, new_filename))  # 保存文件到upload目录
+
+
+        return jsonify({"errno": 0, "errmsg": "上传成功"})
+    else:
+        return jsonify({"errno": 1001, "errmsg": "上传失败"})
+
+@app.route('/download/<filename>', methods=['GET','POST'], strict_slashes=False)
+def download(filename):
+    print(filename)
+    # if request.method=="GET":
+    #     if os.path.isfile(os.path.join('upload', filename)):
+    #         return send_from_directory('upload',filename.encode('utf-8').decode('utf-8'), as_attachment=True)
+    #     abort(404)
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8888)
